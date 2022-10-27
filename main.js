9f930fbdb393e2ba753d10e4e93c891e63a108c6
riskafitrioktavianti <106950909+riskafitrioktavianti@users.noreply.github.com>
2022-10-27 15:54:44 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ function popupku() {
   var pWm = document.createElement("a");
   pWm.setAttribute("href", "https://instagram.com/riska_fitri_oktavianti?igshid=YmMyMTA2M2Y=");
   pWm.setAttribute("style", "text-decoration: none; color: white; opacity: .5; position: fixed; bottom : 10px; left: 10px");
-  pWm.innerHTML = "Instagram : @riska_fitri_oktavianti";
+  pWm.innerHTML = "From Bayu Saputra";
   body.appendChild(pWm);
 }
